b0a29f77148d1f78b88b6fb33999a680faa59d7c
leo2010ahmed-max <leo2010ahmed@gmail.com>
2026-05-03 19:05:54 +0100

    Add players.js with stats of 8 professional padel players

---
+++ players.js
@@ -0,0 +1,12 @@
+const players = [
+    { name: "Juan Lebrón", country: "Spain", ranking: 1 },
+    { name: "Paquito Navarro", country: "Spain", ranking: 2 },
+    { name: "Alejandro Galán", country: "Spain", ranking: 3 },
+    { name: "Fernando Belasteguin", country: "Argentina", ranking: 4 },
+    { name: "Sanyo Gutiérrez", country: "Argentina", ranking: 5 },
+    { name: "Bela", country: "Argentina", ranking: 6 },
+    { name: "Marta Marrero", country: "Spain", ranking: 7 },
+    { name: "Lucia Sainz", country: "Spain", ranking: 8 }
+];
+
+export default players;
